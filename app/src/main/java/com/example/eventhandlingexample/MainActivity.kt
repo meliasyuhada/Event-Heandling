cfed436bb997a21cf59bba03180fca8155b7bbf8
Melia Syhada <meliasyuhada5@gmail.com>
2022-04-09 11:41:01 +0700

    Event Headling

---
+++ app/src/main/java/com/example/eventhandlingexample/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.eventhandlingexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btnDisplayMessage.setOnClickListener {
+            Toast.makeText(applicationContext, "Hello World", Toast.LENGTH_LONG).show()
+        }
+
+        btnDisplayMessage.setOnLongClickListener {
+            Snackbar.make(root_layout, "Long clik", Snackbar.LENGTH_LONG).show()
+            true }
+    }
+}
